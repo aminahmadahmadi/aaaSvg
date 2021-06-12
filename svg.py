e21eac78712ce0693d665fae1a947ea1d276e248
aminahamadahmadi <aminahmadahmadi@gmail.com>
2021-06-12 21:52:50 +0430

    add codes for test

---
+++ svg.py
@@ -0,0 +1,19 @@
+﻿class Svg:
+    count = 0
+
+    def __init__(self, name, w, h):
+        self.name = name
+        self.width = w
+        self.height = h
+        self.styles = []
+        self.objects = []
+        Svg.count += 1
+
+    def addStyle(self, className, css):
+        self.styles.append(f'.{className} {{{css}}}')
+        print(self.styles[-1])
+
+
+# Test Code ----------------------------------------
+myfile = Svg('myfile', 100, 100)
+myfile.addStyle('bg', 'stroke: #00f; fill: #00f;')
