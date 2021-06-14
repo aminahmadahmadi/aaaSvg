87294217dbf92234d2760a3732fbace92d222dd1
aminahamadahmadi <aminahmadahmadi@gmail.com>
2021-06-14 21:40:22 +0430

    addObjectText and addCircle function Added

--- svg.py
+++ svg.py
@@ -13,7 +13,28 @@
         self.styles.append(f'.{className} {{{css}}}')
         print(self.styles[-1])
 
+    def addObjectText(self, objText):
+        self.objects.append(objText)
+
+    def manageAttrs(attrs):
+        _attrs = ''
+        for key in attrs.keys():
+            if key == 'style':
+                _attrs += f'class="{attrs[key]}" '
+            else:
+                _attrs += f'{key}="{attrs[key]}" '
+
+        return _attrs
+
+    def addCircle(self, cx, cy, r, **attrs):
+        print(attrs)
+        circleStr = f'<circle cx="{cx}" cy="{cy}" r="{r}" {Svg.manageAttrs(attrs)}/>'
+
+        print(circleStr)
+        self.addObjectText(circleStr)
+
 
 # Test Code ----------------------------------------
 myfile = Svg('myfile', 100, 100)
-myfile.addStyle('bg', 'stroke: #00f; fill: #00f;')
+myfile.addStyle('style', 'stroke: #00f; fill: #00f;')
+myfile.addCircle(50, 50, 20, style='style', id='salam', fill='none')
