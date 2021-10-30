867684b99afbfa367b2bcc27f3e801435edd2f4a
Amin Ahmad Ahmadi <a.ahmadi@KANDA.idea>
2021-10-30 12:12:22 +0330

    make python package and setuptool

---
+++ setup.py
@@ -0,0 +1,9 @@
+﻿from setuptools import setup
+
+setup(
+    name="aaaSvg",
+    version="0.1",
+    description="Package for making svg file with python code",
+    author="Amin Ahmad Ahmadi",
+    packages=["aaaSvg"]
+)
