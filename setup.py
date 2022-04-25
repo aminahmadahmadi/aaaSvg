53ac1ce532dd81812994510eb73453b0ca392bc1
aminahmadahmadi <aminahmadahmadi@gmail.com>
2022-04-25 12:55:22 +0430

    version updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 setup(
     name="aaaSvg",
-    version="0.1.6",
+    version="0.1.7",
     description="Package for making svg file with python code",
     author="Amin Ahmad Ahmadi",
     packages=["aaaSvg"]
