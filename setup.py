52d696573b68b5852561754137c6141fa0b940c0
Amin Ahmad Ahmadi <aminahmadahmadi@Amins-MacBook-Air.local>
2022-05-26 09:55:31 +0430

    change setup file py to cfg

--- setup.py
+++
@@ -1,9 +0,0 @@
-﻿from setuptools import setup
-
-setup(
-    name="aaaSvg",
-    version="0.1.8",
-    description="Package for making svg file with python code",
-    author="Amin Ahmad Ahmadi",
-    packages=["aaaSvg"]
-)
